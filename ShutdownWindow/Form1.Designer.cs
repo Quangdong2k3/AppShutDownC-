ce9b70d266e4b3f57f4de3b0fdcd6ec7524845ef
dong dz <dongtay819@gmail.com>
2023-11-11 20:42:50 +0700

    Thêm chức năng mới

---
+++ ShutdownWindow/Form1.Designer.cs
@@ -0,0 +1,280 @@
+﻿namespace ShutdownWindow
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.cbbMethod = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.numHour = new System.Windows.Forms.NumericUpDown();
+            this.numMin = new System.Windows.Forms.NumericUpDown();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.lblRTimeNow = new System.Windows.Forms.Label();
+            this.lblTimeLeft = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.numTimeAlert = new System.Windows.Forms.NumericUpDown();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.label7 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numHour)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numMin)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numTimeAlert)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.cbbMethod);
+            this.groupBox1.Location = new System.Drawing.Point(39, 84);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(388, 100);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Chức năng";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.label7);
+            this.groupBox2.Controls.Add(this.numTimeAlert);
+            this.groupBox2.Controls.Add(this.checkBox1);
+            this.groupBox2.Controls.Add(this.lblTimeLeft);
+            this.groupBox2.Controls.Add(this.lblRTimeNow);
+            this.groupBox2.Controls.Add(this.label5);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.numMin);
+            this.groupBox2.Controls.Add(this.numHour);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Location = new System.Drawing.Point(39, 205);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(388, 165);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Hẹn giờ";
+            // 
+            // cbbMethod
+            // 
+            this.cbbMethod.FormattingEnabled = true;
+            this.cbbMethod.Items.AddRange(new object[] {
+            "Shutdown"});
+            this.cbbMethod.Location = new System.Drawing.Point(26, 58);
+            this.cbbMethod.Name = "cbbMethod";
+            this.cbbMethod.Size = new System.Drawing.Size(208, 21);
+            this.cbbMethod.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(23, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(163, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Bạn muốn chọn chức năng nào?";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(23, 35);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(65, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Tắt máy sau";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(165, 35);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(21, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "giờ";
+            // 
+            // numHour
+            // 
+            this.numHour.Location = new System.Drawing.Point(110, 33);
+            this.numHour.Name = "numHour";
+            this.numHour.Size = new System.Drawing.Size(49, 20);
+            this.numHour.TabIndex = 4;
+            // 
+            // numMin
+            // 
+            this.numMin.Location = new System.Drawing.Point(192, 33);
+            this.numMin.Name = "numMin";
+            this.numMin.Size = new System.Drawing.Size(47, 20);
+            this.numMin.TabIndex = 5;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(23, 66);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(57, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Thời gian :";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(23, 95);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(49, 13);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Còn Lại :";
+            // 
+            // lblRTimeNow
+            // 
+            this.lblRTimeNow.AutoSize = true;
+            this.lblRTimeNow.Location = new System.Drawing.Point(110, 66);
+            this.lblRTimeNow.Name = "lblRTimeNow";
+            this.lblRTimeNow.Size = new System.Drawing.Size(49, 13);
+            this.lblRTimeNow.TabIndex = 8;
+            this.lblRTimeNow.Text = "00:00:00";
+            // 
+            // lblTimeLeft
+            // 
+            this.lblTimeLeft.AutoSize = true;
+            this.lblTimeLeft.Location = new System.Drawing.Point(110, 95);
+            this.lblTimeLeft.Name = "lblTimeLeft";
+            this.lblTimeLeft.Size = new System.Drawing.Size(49, 13);
+            this.lblTimeLeft.TabIndex = 9;
+            this.lblTimeLeft.Text = "00:00:00";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(26, 128);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(123, 17);
+            this.checkBox1.TabIndex = 10;
+            this.checkBox1.Text = "Nhắc nhở sau (phút)";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // numTimeAlert
+            // 
+            this.numTimeAlert.Location = new System.Drawing.Point(168, 125);
+            this.numTimeAlert.Name = "numTimeAlert";
+            this.numTimeAlert.Size = new System.Drawing.Size(66, 20);
+            this.numTimeAlert.TabIndex = 11;
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(449, 142);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 2;
+            this.btnStart.Text = "Bắt đầu";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Location = new System.Drawing.Point(449, 184);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(75, 23);
+            this.btnCancel.TabIndex = 3;
+            this.btnCancel.Text = "Hủy";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(245, 35);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(28, 13);
+            this.label7.TabIndex = 13;
+            this.label7.Text = "phút";
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // timer2
+            // 
+            this.timer2.Interval = 1000;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(586, 401);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numHour)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numMin)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numTimeAlert)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox cbbMethod;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label lblTimeLeft;
+        private System.Windows.Forms.Label lblRTimeNow;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.NumericUpDown numMin;
+        private System.Windows.Forms.NumericUpDown numHour;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.NumericUpDown numTimeAlert;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer timer2;
+    }
+}
+
---
+++ ShutdownWindow/Form1.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.Runtime.InteropServices;   
+namespace ShutdownWindow
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private int hours;
+        private int mins;
+        private int seconds;
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            lblRTimeNow.Text = DateTime.Now.ToString("hh:mm:ss tt");
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            cbbMethod.SelectedIndex = 0;
+            numTimeAlert.Visible = false;
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            numTimeAlert.Visible = checkBox1.Checked;
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            if(numMin.Value<1 && numHour.Value==0)
+            {
+                MessageBox.Show("Bạn chưa chọn thời gian.", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }    
+            else
+            {
+                hours = (int)numHour.Value;
+                mins = (int)numMin.Value;
+
+                timer2.Start();
+                timer1.Enabled = true;
+                btnStart.Enabled = false;
+                btnCancel.Enabled = true;
+
+            }
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            if (hours > 0 | mins > 0 | seconds > 0)
+            {
+                if (mins == 0 && hours > 0) { mins = 59; hours = hours - 1; }
+                if (seconds == 0 && mins > 0) { seconds = 59; mins = mins - 1; }
+                seconds = seconds - 1;
+            }
+            numHour.Value = hours;
+            numMin.Value = mins;
+            //hien thi thoi gian con lai
+            lblTimeLeft.Text = string.Format("{0}:{1}:{2}", formatHour(hours), formatHour(mins), formatHour(seconds));
+
+            if (checkBox1.Checked)
+            {
+                string msg = string.Format("Máy tính của bạn sẽ {0} sau {1} phút nữa. Vui lòng lưu tài liệu đang dở và đóng cửa sổ làm việc. ",cbbMethod,mins);
+                if (mins == numTimeAlert.Value && seconds == 0) MessageBox.Show(msg, "Cảnh Báo", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            // thuc hien chuc nang khi het gio
+            if(hours==0 && seconds==0 && mins == 0)
+            {
+                timer2.Stop();
+                int select = cbbMethod.SelectedIndex;
+                switch(select)
+                {
+                    default:
+                        case 0:
+                        MessageBox.Show("Shutdown");
+                        Process.Start("shutdown", "/s /t 0");
+                        break;
+                                       
+                }    
+            }
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            timer2.Stop();
+            btnStart.Enabled = true;
+            btnCancel.Enabled = false;
+            lblTimeLeft.Text = "00:00:00";
+        }
+        //dinh dang gio
+        private string formatHour(int s)
+        {
+            string t = s.ToString();
+            return s < 10 ? "0" + t : t;
+        }
+    }
+}
